b4e86d1d4b8344a8c0608b75aa7106a9498887c0
Filippo Valle <filippo.valle@edu.unito.it>
2018-03-24 21:55:23 +0100

    gitignore

--- tests/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/filippo/Developer/neuralC/tests/lib/googletest-master
-# Build directory: /Users/filippo/Developer/neuralC/tests/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
